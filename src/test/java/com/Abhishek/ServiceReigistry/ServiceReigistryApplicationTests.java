b3bca51e2b6ee5c3a2de72d607ea94dd321756b9
Abhikumarimhere <abhishekkumar33822@gmail.com>
2024-12-24 01:56:24 +0530

    sMS

---
+++ src/test/java/com/Abhishek/ServiceReigistry/ServiceReigistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Abhishek.ServiceReigistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceReigistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
